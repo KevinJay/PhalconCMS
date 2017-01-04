fbdbed9621e7e4e1819953da0cfdc2abe69ce421
Marser <kwj_web@163.com>
2017-01-04 10:05:02 +0800

    删除无用的文件

--- app/backend/repositories/Users.php
+++ app/backend/repositories/Users.php
@@ -46,7 +46,7 @@ class Users extends BaseRepository{
         $userinfo = $user -> toArray();
         /** 校验密码 */
         if(!$this -> getDI() -> get('security') -> checkHash($password, $userinfo['password'])){
-            throw new \Exception('密码错误，请重新输入');
+            throw new \Exception('用户名或密码错误，请重新输入');
         }
         /** 设置session */
         unset($userinfo['password']);
